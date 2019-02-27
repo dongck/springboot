c89a9b584c4f7215ae691e961e9dc259f20760d4
dongck <525880573@qq.com>
2019-02-27 17:03:03 +0800

    org.apache.maven.archiver.MavenArchiver.getManifest错误解决方法:pom.xml中增加： <maven-jar-plugin.version>2.6</maven-jar-plugin.version>

---
+++ base_demo_01/src/main/java/com/dongck/BaseDemo01Application.java
@@ -0,0 +1,13 @@
+package com.dongck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseDemo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseDemo01Application.class, args);
+	}
+
+}
